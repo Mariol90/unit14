85b32d4a26809a3e4a984ffdbc365ff1826b2d0f
Mariol90 <97477260+Mariol90@users.noreply.github.com>
2022-04-14 17:18:50 -0400

    Add files via upload

---
+++ Exercise.js
@@ -0,0 +1,76 @@
+
+// Same keys and values
+
+// // old way_______________________________________________
+// function createInstructor(firstName, lastName){
+//   return {
+//     firstName: firstName,
+//     lastName: lastName
+//   }
+// }
+// // New way_______________________________________________
+// function createInstructor(firstName, lastName){
+//     return {
+//       firstName,
+//       lastName
+//     }
+//   }
+
+// Computed Property Names
+
+// // old way_______________________________________________
+// var favoriteNumber = 42;
+// var instructor = {
+//   firstName: "Colt"
+// }
+// instructor[favoriteNumber] = "That is my favorite!"
+// // New way_______________________________________________
+// var favoriteNumber = 42;
+// var instructor = {
+//   firstName: "Colt",
+// [favoriteNumber]:"That is my favorite!"
+// }
+// // old way_______________________________________________
+
+// Object Methods
+
+// var instructor = {
+//   firstName: "Colt",
+//   sayHi: function(){
+//     return "Hi!";
+//   },
+//   sayBye: function(){
+//     return this.firstName + " says bye!";
+//   }
+// }
+// // New way_______________________________________________
+
+// var instructor = {
+//     firstName: "Colt",
+//     sayHi(){
+//       return "Hi!";
+//     },
+//     sayBye(){
+//       return this.firstName + " says bye!";
+//     }
+//   }
+
+//   createAnimal function
+const d = createAnimal("dog", "bark", "Woooof!")
+// {species: "dog", bark: ƒ}
+ d.bark()  //"Woooof!"
+
+const s = createAnimal("sheep", "bleet", "BAAAAaaaa")
+// {species: "sheep", bleet: ƒ}
+ s.bleet() //"BAAAAaaaa"
+
+  function createAnimal(species,verb,noise){
+    return {
+        species,
+        [verb]:"f",
+        [verb](){
+         return noise;
+         }
+      }
+    
+  ;}
